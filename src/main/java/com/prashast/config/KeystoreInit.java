ccab0f9f639addbdecbacdb735bf1fa9766565d2
Prashast Saxena <coolprashi06@gmail.com>
2019-10-23 00:42:35 +0530

    SSL support conditionally

--- src/main/java/com/prashast/config/KeystoreInit.java
+++ src/main/java/com/prashast/config/KeystoreInit.java
@@ -1,9 +1,11 @@
 package com.prashast.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.context.embedded.Ssl;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +13,8 @@ import org.springframework.stereotype.Component;
 https://stackoverflow.com/questions/44969873/spring-boot-key-store-password-set-in-code
  */
 
-@Component
+@Configuration
+@ConditionalOnProperty(name = "cloud.app", havingValue = "false")
 public class KeystoreInit {
 
     private final Environment environment;
@@ -26,14 +29,20 @@ public class KeystoreInit {
         final ServerProperties serverProperties = new ServerProperties();
         final Ssl ssl = new Ssl();
         final String keystorePassword = getKeystorePassword();
+        final String keystore = getKeystore();
         ssl.setKeyPassword(keystorePassword);
-        System.setProperty("server.ssl.key-store-password", keystorePassword);
+        ssl.setKeyStore(keystore);
+        ssl.setEnabled(true);
         serverProperties.setSsl(ssl);
         return serverProperties;
     }
 
     private String getKeystorePassword() {
         return System.getProperty("keystorePassword");
+    }
+
+    private String getKeystore() {
+        return System.getProperty("keystoreFile");
     }
 
 }
